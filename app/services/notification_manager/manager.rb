9d3dd29430cdc22d68d40c100c11d4bebcbe3092
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-04-22 15:11:49 -0400

    alignent crap

--- app/services/notification_manager/manager.rb
+++ app/services/notification_manager/manager.rb
@@ -28,31 +28,31 @@ module NotificationManager
 			byebug
 		end
 
-	    def send_email(constructed_email)
-	      # send the actual email
-	      constructed_email.deliver
-	    end
+		def send_email(constructed_email)
+		  # send the actual email
+		  constructed_email.deliver
+		end
 
-	    def construct_email(target_email, changes)
-	      #use a passed array/hash type to construct the email body for all 
-	      #changes concerning a user
-	      body = prepare_body(changes)
-	      #mailer here
-	    end
-	    def prepare_body(changes)
-	    	#create html needed for the table with loop
-	    	#will need helper methods here to write the different changes
-	    end
-	# dont need this
-	    def queue_job(change_id)
-	      # start the redis worker with needed delay
-	      Resque.enqueue(MakeChange, change_id)
-	    end
+		def construct_email(target_email, changes)
+		  #use a passed array/hash type to construct the email body for all 
+		  #changes concerning a user
+		  body = prepare_body(changes)
+		  #mailer here
+		end
+		def prepare_body(changes)
+			#create html needed for the table with loop
+			#will need helper methods here to write the different changes
+		end
+		# dont need this
+		def queue_job(change_id)
+		  # start the redis worker with needed delay
+		  Resque.enqueue(MakeChange, change_id)
+		end
 
-	    def cancel_change(change_id)
+		def cancel_change(change_id)
 
-	      # cancel the change in the model
-	      # notification.change_cancelled = true
-	    end
+		  # cancel the change in the model
+		  # notification.change_cancelled = true
+		end
 	end
 end
