4d974e2ee6742a02fb867e5502b14b7a7c05cead
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-11-15 16:05:20 -0500

    correct spacing in code

--- app/jobs/change_manager/begin_change.rb
+++ app/jobs/change_manager/begin_change.rb
@@ -1,13 +1,13 @@
 require 'yaml'
 module ChangeManager
-	class BeginChange
-		def self.queue
-			:change
-		end
+  class BeginChange
+    def self.queue
+      :change
+    end
 
-		def self.perform(change_id)
-			config ||= YAML.load_file(File.join(Rails.root, 'config/change_manager_config.yml'))
-			config['manager_class'].constantize.process_change(change_id)
-		end
-	end
+    def self.perform(change_id)
+      config ||= YAML.load_file(File.join(Rails.root, 'config/change_manager_config.yml'))
+      config['manager_class'].constantize.process_change(change_id)
+    end
+  end
 end
--- app/mailers/change_manager/notification_mailer.rb
+++ app/mailers/change_manager/notification_mailer.rb
@@ -1,33 +1,32 @@
 require 'yaml'
 
 module ChangeManager
-	class NotificationMailer < ActionMailer::Base
-		CONFIG ||= YAML.load_file(File.join(Rails.root, 'config/change_manager_config.yml'))
-		def construct_email(changes)
-			@body = prepare_body(changes)
-			mail(
-				to: changes.first.target,
-				from: changes.first.owner,
-				subject: 'Recent Updates from ' + CONFIG['site_name'],
-				)
-		end
+  class NotificationMailer < ActionMailer::Base
+    CONFIG ||= YAML.load_file(File.join(Rails.root, 'config/change_manager_config.yml'))
+    def construct_email(changes)
+      @body = prepare_body(changes)
+      mail(
+        to: changes.first.target,
+        from: changes.first.owner,
+        subject: 'Recent Updates from ' + CONFIG['site_name'],)
+    end
 
-		def send_email(constructed_email)
-			constructed_email.deliver
-		end
+    def send_email(constructed_email)
+      constructed_email.deliver
+    end
 
-		def prepare_body(changes)
-			header = '<table><th><td>Change Owner</td><td>Change Context</td><td>Change</td><td>Time</td></th>'
-			body = ''
-			footer = '</table>'
-			changes.each do |change|
-				body += '<tr><td>' + 
-				change.owner + '</td><td>' + 
-				change.context + '</td><td>' + 
-				change.change_type + '</td><td>' + 
-				change.created_at.to_s + '</td></tr>'
-			end
-			content = header + body + footer
-		end
-	end
+	def prepare_body(changes)
+      header = '<table><th><td>Change Owner</td><td>Change Context</td><td>Change</td><td>Time</td></th>'
+      body = ''
+      footer = '</table>'
+      changes.each do |change|
+        body += '<tr><td>' + 
+        change.owner + '</td><td>' + 
+        change.context + '</td><td>' + 
+        change.change_type + '</td><td>' + 
+        change.created_at.to_s + '</td></tr>'
+      end
+      content = header + body + footer
+    end
+  end
 end
