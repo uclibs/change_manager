93a783074369e3911a09f6cae1785a4c5bcd8752
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2017-03-22 16:24:24 -0400

    refactor begin_change to use ActiveJob, fix stupid git things

--- app/jobs/change_manager/process_change_job.rb
+++ app/jobs/change_manager/process_change_job.rb
@@ -1,11 +1,9 @@
 require 'yaml'
 module ChangeManager
-  class BeginChange
-    def self.queue
-      :change
-    end
-
-    def self.perform(change_id)
+  class ProcessChangeJob < ActiveJob::Base
+  queue_as :change
+  
+    def perform(change_id)
       config ||= YAML.load_file(File.join(Rails.root, 'config/change_manager_config.yml'))
       config['manager_class'].constantize.process_change(change_id)
     end
